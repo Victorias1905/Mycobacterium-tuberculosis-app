8be8f21c2587b63e883636f8d3fc8ca9014a204c
Victorias1905 <102805197+Victorias1905@users.noreply.github.com>
2024-08-20 03:26:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,11 @@ def download_file_from_google_drive(url, destination):
     with open(destination, 'wb') as f:
         f.write(response.content)
 url='https://github.com/Victorias1905/Mycobacterium-tuberculosis-app/blob/main/ne_110m_admin_0_countries.shp'
-africa=gpd.read_file(url)
+response = requests.get(url)
+with open(local_filename, 'wb') as file:
+    file.write(response.content)
+africa = gpd.read_file(local_filename)
+os.remove(local_filename)
 URL_DATASET_1 = 'https://drive.google.com/uc?export=download&id=1fvFcosmNcIxqH0dy56aZU4Cizknm777D'
 URL_DATASET_2 = 'https://drive.google.com/uc?export=download&id=1HnFDhSOKwybtD7r9-IwHlwQ6tYHrVChv'
 URL_DATASET_3 = 'https://drive.google.com/uc?export=download&id=116E6HD17qkspBEyRZOGE-vRpEyKL0JFu'
