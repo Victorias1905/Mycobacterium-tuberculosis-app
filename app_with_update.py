088c4526d09226ef980293492ba09642118244d4
Victorias1905 <102805197+Victorias1905@users.noreply.github.com>
2024-12-13 22:50:54 +0200

    Update app_with_update.py

--- app_with_update.py
+++ app_with_update.py
@@ -194,6 +194,7 @@ def push_to_git():
         st.error(f"Unexpected error: {e}")
     st.write(f"Token: {token[:4]}***")  # Only show the first few characters for security
     st.write(f"Remote URL: {auth_remote}")
+    st.write(f"Token from secrets: {st.secrets['general']['GITHUB_TOKEN'][:4]}***")
 
 
 # Streamlit button to trigger the function
