28f98862c52685564b13ca3f91b376f8ee5bbf7e
Victorias1905 <102805197+Victorias1905@users.noreply.github.com>
2024-12-13 15:38:28 +0200

    Update app_with_update.py

--- app_with_update.py
+++ app_with_update.py
@@ -8,6 +8,8 @@ import unicodedata
 import tiktoken
 import os
 import subprocess
+git config --global user.name "Victorias1905"
+git config --global user.email "victoria.strahova@student.uhasselt.be"
 api_key = st.secrets["general"]["OPENAI_API_KEY"]
 
 with open("latest_model.json", "r") as file:
