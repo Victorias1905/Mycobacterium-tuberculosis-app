63d385e7fb84fe6071c2f5db81d647f72ca128a1
Victorias1905 <102805197+Victorias1905@users.noreply.github.com>
2025-01-03 22:10:22 +0200

    Update app_gpt.py

--- app_gpt.py
+++ app_gpt.py
@@ -214,18 +214,18 @@ if preprocess_button:
     else:
         st.warning("Please upload at least one PDF file.")
        
-if update_button:
-    ids = list(range(len(chunks)))  # Generate unique IDs
-    for id,vector, chunk, metadata in zip(ids,vectors_float, chunks, chunk_metadata):
-        data_to_insert = {
-            "id": id,
-            "vector": vector,
-            "text": chunk,
-            "metadata": metadata,  
-        }
-    
-        client_milvus.insert(collection_name="Mycobacterium", data=data_to_insert)
-    st.write("Data inserted successfully!")
+    if update_button:
+        ids = list(range(len(chunks)))  # Generate unique IDs
+        for id,vector, chunk, metadata in zip(ids,vectors_float, chunks, chunk_metadata):
+            data_to_insert = {
+                "id": id,
+                "vector": vector,
+                "text": chunk,
+                "metadata": metadata,  
+            }
+        
+            client_milvus.insert(collection_name="Mycobacterium", data=data_to_insert)
+        st.write("Data inserted successfully!")
     
 
 
