6e6b872ca3469281cce99eb4ac3985480898a088
Victorias1905 <102805197+Victorias1905@users.noreply.github.com>
2025-01-03 20:41:10 +0200

    Update app_gpt.py

--- app_gpt.py
+++ app_gpt.py
@@ -18,8 +18,7 @@ zilliz_token = "641b977aa113eb7f095c50a472347f9f089f6ee89e1346d3ea316db3223c8cf9
 collection_name = "Mycobacterium"
 embedding_field = "vector"  # Field name for embeddings in Zilliz
 model="ft:gpt-4o-mini-2024-07-18:mtbc-project::Akwtgx7I"
-client = MilvusClient(uri="https://in03-03d63efede22046.serverless.gcp-us-west1.cloud.zilliz.com",
-                              token="641b977aa113eb7f095c50a472347f9f089f6ee89e1346d3ea316db3223c8cf9b4f42bfd705ccb1fad8d7b00d62f1b27bfe8a59e")
+
 # Connect to Zilliz Cloud
 try:
     connections.connect("default", uri=zilliz_uri, token=zilliz_token)
@@ -184,10 +183,11 @@ def chunk_documents_with_references(Update_list):
 def generate_embeddings(chunks):
     embeddings = []
     for chunk in chunks:
-        response = openai.embeddings.create(
-        input=chunk,
-        model="text-embedding-3-small")
-        embeddings.append(response)
+      client = openai.OpenAI(api_key=api_key)
+      response = client.embeddings.create(
+      input=chunk,
+      model="text-embedding-3-small")
+      embeddings.append(response)
     
     return embeddings
     
